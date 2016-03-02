0019d301a6c5ae8d24ee736163f9c5b30e8194d0
Ken Tran <loschorts@gmail.com>
2016-03-01 21:32:34 -0800

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 module.exports = {
-    context: __dirname + "/frontend",
-    entry: "./routes.jsx",
-    output: {
-        path: __dirname + "/app/assets/javascripts",
-        filename: "bundle.js"
-    },
-    devtool: "source-map"
+  context: __dirname + "/frontend",
+  entry: "./swell.jsx",
+  output: {
+      path: __dirname + "/app/assets/javascripts",
+      filename: "bundle.js"
+  },
+  devtool: "source-map",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  resolve: {
+  // you can now require('file') instead of require('file.coffee')
+  extensions: ['', '.js', '.jsx', '.json', '.coffee'] 
+  }
+	  
+
 };
