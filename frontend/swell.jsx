b3090594c90aba6e6e4890524f24e000a1f5b249
Ken Tran <loschorts@gmail.com>
2016-03-01 22:15:55 -0800

    config

--- frontend/swell.jsx
+++ frontend/swell.jsx
@@ -1,14 +1,18 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-var Router = require("react-router");
-var Route = Router.route;
-var browserHistory = Router.broswerHistory;
+var Router = require("react-router").Router;
+var Route = require("react-router").Route;
+var browserHistory = require("react-router").broswerHistory;
+
+var App = require('./components/app');
 
 var routes = (
-	<Router>
+	<Router history={browserHistory}>
 		<Route path="/" component={App}/>
 	</Router>
 	);
 
-ReactDOM.render(routes, document.getElementById("react"));
+document.addEventListener("DOMContentLoaded", function(){
+	ReactDOM.render(routes, document.getElementById("swell"));
+});
