ec0e92198dcb784a1b2e267c4b6a375f2e649e36
Ken Tran <loschorts@gmail.com>
2016-03-06 18:03:30 -0800

    stylesheets working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,2 @@
 class PagesController < ApplicationController
-	def splash
-		@page_specific_stylesheet = ActionController::Base.helpers.asset_path("splash.css")
-	end
-
-	def app
-		@page_specific_stylesheet = ActionController::Base.helpers.asset_path("app.css")
-	end
 end
