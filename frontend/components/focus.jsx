c7e67e006fc77810c0bfd7ac1c2b48b08e708c03
Ken Tran <loschorts@gmail.com>
2016-03-22 20:09:39 -0700

    focus wishlist

--- frontend/components/focus.jsx
+++ frontend/components/focus.jsx
@@ -1,11 +1,46 @@
+// libraries
 var React = require("react");
+
+// components
 var WeatherOverlay = require("./weather_overlay");
 
+// modules 
+var CurrentUserState = require("../modules/current_user_state");
+var CheckIfExists = require("../modules/check_if_exists");
+
 var Focus = React.createClass({
+	componentDidMount: function(){
+		SpotApiUtil.fetchSpot(this.props.spotId);
+		SpotStore.addListener(this.updateSpot);
+	},
+	updateSpot: function(){
+		ForecastStore.addListener(this.updateForecast);
+		ForecastApiUtil.fetchForecast();
+		this.setState({spot: SpotStore.find(this.props.spotId)});
+	},
+	updateForecast: function(){
+		this.setState({forecast: ForecastStore.find(this.props.spotId)});
+	},
+	spotInfo: function(){
+		if (this.state.spot) {
+			return (
+
+				);
+		}
+	},
+	forecastInfo: function(){
+		if (this.state.forecast) {
+			return (
+				
+				);
+		}
+	},
 	render: function(){
 		return (
 			<div id="focus" className="group">
 				<WeatherOverlay/>
+				{this.spotInfo()}
+				{this.forecastInfo()}
 			</div>
 		);
 	}
