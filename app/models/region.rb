44ed555e1c122ea92120eb6deff167f516ccd90b
Ken Tran <loschorts@gmail.com>
2016-05-04 23:37:25 -0700

    change region unique validation to uniqueness

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,5 +1,5 @@
 class Region < ActiveRecord::Base
-	validates :name, :description, presence: true, unique: true
+	validates :name, :description, presence: true, uniqueness: true
 	has_many :counties
 	has_many :spots, through: :counties
 end
--- frontend/components/collection.jsx
+++ frontend/components/collection.jsx
@@ -8,19 +8,32 @@ var Collection = React.createClass({
 		return (
 			<section className="collection">
 				<div className="collection-header">
-					<h2> All signs point right </h2>
+					<h2> All signs point right. </h2>
 					<p> Regular footers rejoice at these right-hand point breaks. </p>
 				</div>
 				<div className="thumbnail-row">
-					<div className="thumbnail" style={style}/>
-					<div className="thumbnail" style={style}/>
-					<div className="thumbnail" style={style}/>
+					<div className="thumbnail" style={style}>
+						<h3>Pleasure Point</h3>
+					</div>
+					<div className="thumbnail" style={style}>
+						<h3>The Hook</h3>
+					</div>
+					<div className="thumbnail" style={style}>
+						<h3>Four Mile</h3>
+					</div>
 				</div>
 				
 				<div className="thumbnail-row">
-					<div className="thumbnail" style={style}/>
-					<div className="thumbnail" style={style}/>
-					<div className="thumbnail" style={style}/>
+					<div className="thumbnail" style={style}>
+						<h3>Steamer Lane</h3>
+					</div>
+					<div className="thumbnail" style={style}>
+						<h3>Rockaway</h3>
+					</div>
+					<div className="thumbnail double" style={style}>
+						<h3>Capitola</h3>
+					</div>
+
 				</div>
 			</section>
 			);
