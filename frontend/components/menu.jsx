01b14f24a57028d155ed5dffd98a3d8feaf16068
Ken Tran <loschorts@gmail.com>
2016-05-04 19:57:42 -0700

    add styling to menu

---
+++ frontend/components/menu.jsx
@@ -0,0 +1,31 @@
+var React = require("react");
+
+var Menu = React.createClass({
+	getInitialState: function(){
+		return {showing: false};
+	},
+	toggleMenu: function(){
+		this.setState({showing: !this.state.showing});
+	},
+	menuItems: function(){
+		if (this.state.showing) {			
+			return(
+				<div className="menu-list">
+					<div className="menu-item">Login</div>
+					<div className="menu-item">Sign Up</div>
+				</div>
+				);
+		}
+	},
+	render: function(){
+		return(
+
+			<div className="menu">
+				<div className="menu-button" onClick={this.toggleMenu} >Menu</div>
+				{this.menuItems()}
+			</div>
+			);
+	}
+});
+
+module.exports = Menu;
--- frontend/components/nav_bar.jsx
+++ frontend/components/nav_bar.jsx
@@ -1,10 +1,7 @@
 var React = require("react");
 var CurrentUserState = require("../modules/current_user_state");
-var CheckIfExists = require("../modules/check_if_exists");
-var NavBarForm = require("./nav_bar_form");
-var MenuItem = require("./menu_item");
-var browserHistory = require("react-router").browserHistory;
 var UserApiUtil = require("../utils/user_api_util");
+var Menu = require("./menu");
 
 var NavBar = React.createClass({
 	getInitialState: function(){
@@ -24,69 +21,11 @@ var NavBar = React.createClass({
 		return (
 			<nav id="nav-bar" className={this.state.scrolled}>
 				<div id="nav-logo">Logo</div>
-				<div id="nav-menu">Button</div>
+				<Menu/>
 			</nav>
 			);
 	}
 
 });
-
-
-
-// var NavBar = React.createClass({
-// 	mixins: [CurrentUserState, CheckIfExists],
-// 	handleClick: function(e){
-// 		var formAction = e.currentTarget.text.split(" ")[0];
-// 		switch (formAction) {
-// 			case "logout":
-// 				UserApiUtil.logout();
-// 				this.setState({formAction: undefined});
-// 				break;
-// 			case "guest": 
-// 				this.setState({formAction: undefined});
-// 				UserApiUtil.guestLogin();
-// 				break;
-// 			default: 
-// 				this.setState({formAction: formAction});
-// 				break;
-// 		}
-// 	},
-// 	clearForm: function(){
-// 		this.setState({formAction: undefined});
-// 	},
-// 	preRender: function(){
-// 		if (this.state.user){
-// 			this.greeting = <a href="#">{"hi there, " + this.state.user.username}</a>
-// 			this.signIn = <MenuItem href="login" text="switch accounts" onClick={this.handleClick}/>
-// 			this.signOut = <MenuItem href="logout" text="logout" onClick={this.handleClick}/>
-// 		} else {
-// 			this.greeting = <a href="#">hi there</a>;
-// 			this.signIn = <MenuItem href="login" text="login" onClick={this.handleClick}/>
-// 			this.signOut = <MenuItem href="new" text="create account" onClick={this.handleClick}/>
-// 		}
-// 		if (this.state.formAction){
-// 			this.form = <NavBarForm action={this.state.formAction} hide={this.clearForm}/>
-// 		} else {
-// 			this.form = undefined;
-// 		}
-// 	},
-// 	render: function(){
-// 		this.preRender();
-// 		return (
-// 			<nav className="group">
-// 				<div className="nav-item left nav-icon"><a href="#"> swell </a></div>
-// 				<div className="nav-item right menu group">
-// 					<div className="menu-header">{this.greeting}</div>
-// 					<ul className="nav-item menu-items group">
-// 						{this.signIn}
-// 						{this.signOut}
-// 						<MenuItem href="guest" text="guest login" onClick={this.handleClick}/>
-// 					</ul>
-// 				</div>
-// 				{this.form}
-// 			</nav>
-// 		);
-// 	}
-// });
 
 module.exports = NavBar;
