a91e7f6623049ec70a7be4adde37e0ae8b921198
Ken Tran <loschorts@gmail.com>
2016-05-09 20:51:33 -0700

    hook up map component to google api

--- frontend/components/focus.jsx
+++ frontend/components/focus.jsx
@@ -12,6 +12,7 @@ var CheckIfExists = require("../modules/check_if_exists");
 var Focus = React.createClass({
 	mixins: [CurrentUserState, CurrentForecastState, CheckIfExists],
 	render: function(){
+		console.log(this.state);
 		return (
 			<div id="focus">
 				<header id="focus-jumbotron">
@@ -20,7 +21,10 @@ var Focus = React.createClass({
 						<h2 className="shape-full">{this.returnIf("this.state.currentForecast.shape_full", "loading shape...")}</h2>
 						<h2 className="size">{this.returnIf("this.state.currentForecast.size", "loading size...")}</h2>	
 					</div>
-					<Map/>
+					<Map 
+						lat={this.returnIf("state.spot.lat")} 
+						lng={this.returnIf("state.spot.lng")}
+					/>
 				</header>
 				
 				<main id="focus-main" >
--- frontend/components/map.jsx
+++ frontend/components/map.jsx
@@ -1,6 +1,21 @@
 var React = require("react");
 
 var Map = React.createClass({
+	// componentDidMount: function(){
+ //    this.map = new google.maps.Map(document.getElementById('focus-map'), {
+ //      center: {lat: this.props.lat, lng: this.props.lng},
+ //      zoom: 8
+ //  	});
+ //  },
+  componentWillReceiveProps: function(newProps){
+  	if (newProps.lat && newProps.lng){
+	  	this.map = new google.maps.Map(document.getElementById('focus-map'), {
+	      center: {lat: newProps.lat, lng: newProps.lng},
+	      zoom: 14,
+	      mapTypeId: google.maps.MapTypeId.SATELLITE
+	  	});
+  	}
+  },
 	render: function(){
 		return (
 			<div id="focus-map">I am the Map</div>
