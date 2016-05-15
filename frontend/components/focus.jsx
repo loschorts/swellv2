3502f6ff444c59dc76034442ba8e77c71707d74a
Ken Tran <loschorts@gmail.com>
2016-05-14 18:36:07 -0700

    add recentering to focus map ;

--- frontend/components/focus.jsx
+++ frontend/components/focus.jsx
@@ -4,6 +4,8 @@ var React = require("react");
 // components
 var Map = require("./map");
 var Wind = require("./wind");
+var Weather = require("./weather");
+
 // mixins
 var CurrentUserState = require("../modules/current_user_state");
 var CurrentForecastState = require("../modules/current_forecast_state");
@@ -20,9 +22,14 @@ var Focus = React.createClass({
 						<div id="focus-left">
 						</div>
 						<div id="focus-center">
-							<h1 className="spot-name">{this.returnIf("state.spot.name")}</h1>
-							<h2 className="shape-full">{this.returnIf("state.currentForecast.shape_full")}</h2>
-							<h2 className="size">{this.returnIf("state.currentForecast.size")} ft</h2>	
+							<div class="focus-blurb">
+								<h1 className="spot-name" onClick={this.recenter}>
+										{this.returnIf("state.spot.name")}
+								</h1>
+								<h2 className="shape-full">{this.returnIf("state.currentForecast.shape_full")}</h2>
+								<h2 className="size">{this.returnIf("state.currentForecast.size")} ft</h2>	
+							</div>
+							<Weather/>
 						</div>
 						<div id="focus-right">
 							<Wind
@@ -47,6 +54,9 @@ var Focus = React.createClass({
 				</main>
 			</div>
 		);
+	},
+	recenter: function(){
+		this.forceUpdate();
 	}
 })
 
