76a56413200c0cb94c4d2bcd77d4b97ad6361d04
Ken Tran <loschorts@gmail.com>
2016-06-11 18:41:03 -0700

    fill out migration

--- db/migrate/20160612013708_create_favorites.rb
+++ db/migrate/20160612013708_create_favorites.rb
@@ -1,8 +1,13 @@
 class CreateFavorites < ActiveRecord::Migration
   def change
     create_table :favorites do |t|
-
+    	t.integer :user_id, null: false
+    	t.integer :spot_id, null: false
       t.timestamps null: false
     end
+
+    add_index :user_id, unique: true
+    add_index :spot_id, unique: true
+    add_index [:user_id, :spot_id], unique: true
   end
 end
