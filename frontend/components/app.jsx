fa5eef2bc2f2483ccf53ce12353d8c22860f27ac
Ken Tran <loschorts@gmail.com>
2016-06-11 18:36:42 -0700

    mve user fetch to app

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,15 +4,10 @@ var SessionStore = require('../stores/session_store');
 var CurrentUserState = require('../modules/current_user_state');
 var SessionActions = require("../actions/session_actions");
 
-var fetched = false;
-
 var App = React.createClass({
 	mixins: [CurrentUserState],
 	componentDidMount: function(){
-		if (!fetched){
-			SessionActions.query();
-			fetched = true;
-		}
+		SessionActions.query();
 	},
 	render: function(){
 		return (
