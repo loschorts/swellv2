cea4b744fae1b9778fca118a9209f38ac86a5e1a
Ken Tran <loschorts@gmail.com>
2017-02-18 11:46:42 -0800

    map

--- frontend/components/map.jsx
+++ frontend/components/map.jsx
@@ -1,12 +1,19 @@
 var React = require("react");
 
 var Map = React.createClass({
-	// componentDidMount: function(){
- //    this.map = new google.maps.Map(document.getElementById('focus-map'), {
- //      center: {lat: this.props.lat, lng: this.props.lng},
- //      zoom: 8
- //  	});
- //  },
+	componentDidMount: function(){
+	  	this.map = new google.maps.Map(document.getElementById('focus-map'), {
+	      center: {lat: this.props.lat, lng: this.props.lng},
+	      zoom: 17,
+	      mapTypeId: google.maps.MapTypeId.SATELLITE,
+	      disableDefaultUI: true,
+	      scrollwheel: false,
+		    navigationControl: false,
+		    mapTypeControl: false,
+		    scaleControl: false,
+		    draggable: true,
+	  	});
+  },
   componentWillReceiveProps: function(newProps){
   	if (newProps.lat && newProps.lng){
 	  	this.map = new google.maps.Map(document.getElementById('focus-map'), {
